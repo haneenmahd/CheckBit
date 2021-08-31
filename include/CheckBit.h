aa5b0ad431421dfea7a6505d8280926132683d38
Haneen Mahdin <haneenmahdin@gmail.com>
2021-08-31 06:35:55 +0000

    `Core`: fix header bugs

--- include/CheckBit.h
+++ include/CheckBit.h
@@ -1,17 +1,14 @@
-enum BitType {
-  64_BIT,
-  32_BIT
-};
+#include <stdlib.h> 
+typedef enum { BIT_64, BIT_32 } BitType; 
 
-BitType getBitType() {
-  BitType bit_type;
+BitType getBitType() { 
+  BitType bit_type; 
   
-  if (sizeof(size_t) == 8)
-  {
-    bit_type = 64_BIT;
-  } else if (sizeof(size_t) == 4) {
-    bit_type = 32_BIT;
-  }
-
-  return bit_type;
+  if (sizeof(size_t) == 8) { 
+    bit_type = BIT_64; 
+  } else if (sizeof(size_t) == 4) { 
+    bit_type = BIT_32; 
+  } 
+  
+  return bit_type; 
 };
