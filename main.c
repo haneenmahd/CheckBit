e36ba921f485d2d047e1c5a460d20f7aef1f639b
Haneen Mahdin <72091386+haneenmahd@users.noreply.github.com>
2021-08-31 09:47:53 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "CheckBit.h"
+
+int main()
+{
+   BitType bit_type = getBitType();
+
+   if (bit_type == 64_BIT) {
+     printf("Running on 64 Bit");
+   } else it (bit_type == 32_BIT) {
+     printf("Running on 64 Bit");
+   }
+}
